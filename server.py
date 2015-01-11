a96f7ba5aece0b587869c3a4e9048b74d92737ed
Frank Yau <ftyau@ualberta.ca>
2015-01-11 01:47:02 +0100

    first attempt

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import SocketServer
+import os
 # coding: utf-8
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
@@ -17,7 +18,7 @@ import SocketServer
 #
 #
 # Furthermore it is derived from the Python documentation examples thus
-# some of the code is Copyright © 2001-2013 Python Software
+# some of the code is Copyright 2001-2013 Python Software
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
@@ -28,11 +29,55 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
+    def serve_file(self, path, extension):
+        file_content = open(path, "r")
+        if extension == "css" or extension == "html":
+            header = "HTTP/1.1 200 OK\r\n"
+            header += "Content-Type: text/" + extension + "\r\n\r\n"
+        else:
+            header = "HTTP/1.1 200 OK\r\n\r\n"
+        
+        self.request.sendall(header + file_content.read())
+        file_content.close()
+        return
+
+    def file_not_found(self):
+        header = "HTTP/1.1 404 Not Found\r\n"
+        header += "Content-Type: text/html\r\n\r\n"
+
+        content = "<html><body>\n"
+        content += "<h2>File not found</h2>\n"
+        content += "This file does not exist :(\n"
+        content += "</body></html>"
+
+        self.request.sendall(header + content)
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        request_data = self.data.split("\n")
+        get_request = request_data[0].split(" ")
+        file_request = get_request[1]
+
+        if file_request[-1] == "/":
+            file_request += "index.html"
+
+        security_check = file_request.split("/")
+        if ".." in security_check:
+            self.file_not_found()
+            return
+
+        file_extension = file_request.split(".")[-1]
+        file_path = os.getcwd() + "/www" + file_request
+        if (os.path.isfile(file_path)):
+            self.serve_file(file_path, file_extension)
+            return
+        else:
+            self.file_not_found()
+            return
+
+        self.request.sendall(file_path)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
